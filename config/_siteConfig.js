5ef1e23d9fcdba898cfb2cbe9e26fc2bafd96065
yashrajtalan <yashrajtalan@outlook.com>
2019-11-30 13:10:23 +0530

    fixed nuxt.js/@googleAnalytics

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -15,7 +15,11 @@ export default {
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
-    id: process.env.UA-153574414-1 // process.env.GOOGLE_ANALYTICS_ID
+    id: 'UA-153574414-1',
+    debug: {
+      enabled: false,
+      sendHitTask: true
+      } // process.env.GOOGLE_ANALYTICS_ID
   },
   mainMenu: [
     {
